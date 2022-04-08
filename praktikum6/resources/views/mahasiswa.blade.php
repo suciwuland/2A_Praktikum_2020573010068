ff063268ca066cd2032e819c43dc04c5f66409d3
suciwuland <suciwd3@gmail.com>
2022-04-08 15:50:52 +0700

    prak6

--- praktikum6/resources/views/mahasiswa.blade.php
+++ praktikum6/resources/views/mahasiswa.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,23 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
     <title>Data Mahasiswa</title>
-</head>
+</head> --}}
 <body>
-    <div class="container text-center p-4 bg-white">
+    @extends('layout.master')
+    @section('title','Data Mahasiswa')
+    @section('menuMahasiswa','active')
+        @section('content')
+       <div class="container text-center    mt-3 p-4 bg-white">
         <h1 class="mb-3">Data Mahasiswa</h1>
-        <div class="row">
-            <div class="col-sm-8 col-md-6 m-auto">
-                <ol class="list-group">
-                    @forelse($mahasiswa as $val)
-                    <li class="list-group-item">
-                        {{$val}}</li>
-                    @empty
-                    <div class="alert alert-dark d-inline-block">Tidak ada data...
-                    </div>
-                    @endforelse
-                </ol>
+            <div class="row">
+                <div class="col-sm-8 col-md-6 m-auto">
+                    <ol class="list-group">
+                        @forelse ($mahasiswa as $val)
+                        <li class="list-group-item">{{ $val }}</li>
+                        @empty
+                        <div class="alert alert-dark d-inline-block">
+                        Tidak ada data...
+                        </div>
+                        @endforelse
+                    </ol>
+                </div>
             </div>
-        </div>
-    </div>
+    </div> 
+    @stop
+    
 </body>
 </html>
