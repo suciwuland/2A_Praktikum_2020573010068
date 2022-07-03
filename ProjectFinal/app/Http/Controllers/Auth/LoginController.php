a283d5b867d38da113f7103f478f4353fbed27b2
suciwuland <suciwd3@gmail.com>
2022-07-04 00:29:23 +0700

    push praktikum

---
+++ ProjectFinal/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+     /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+   protected function authenticated(Request $request, $user)
+   {
+       if ($user->is_active == 0) {
+           $this->guard()->logout();
+           $request->session()->flush();
+           $request->session()->regenerate();
+
+           Session::flash(trans('auth.user_inactive'), 'error');
+
+           return redirect()->route('login');
+       }
+
+       Session::flash(trans('auth.welcome', ['name' => $user->name]));
+   }
+}
